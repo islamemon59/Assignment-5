b74697c6b49424fbbe4e17e5065a25d39ebe3f4f
islamemon59 <islamemon59@gmail.com>
2025-03-01 18:42:45 +0600

    daynamic page added

--- script/app.js
+++ script/app.js
@@ -1,1 +1,33 @@
-console.log("connect...")
+// get current date 
+const today = new Date();
+let date = today.getDate();
+let month = today.getMonth() + 1;
+let year = today.getFullYear();
+let todayDate =    `${date}-${month}-${year}`;
+// Current date added 
+let newDate = document.getElementById("new-date");
+newDate.innerText = todayDate;
+
+// new today added 
+document.getElementById("new-today").addEventListener("click",function(){
+    window.location.href = "index2.html";
+})
+
+
+const completeBtn = document.querySelectorAll(".complete-btn");
+
+for(let i = 0; i < completeBtn.length; i++){
+    const taskBtn = completeBtn[i];
+    
+    taskBtn.addEventListener("click",function(event){
+        const completeTask = document.querySelectorAll(".task");
+        for(let i = 0; i < completeTask.length; i++){
+            const task = completeTask[i];
+            console.log(task)
+        }
+        if(event.target.innerText === "Completed"){
+            alert("You complete a task");
+        taskBtn.disabled = true;
+        }
+    })
+}
